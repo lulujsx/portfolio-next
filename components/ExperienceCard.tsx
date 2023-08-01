c7b611636ac6ac2d38b7984daf597bb34537ef32
Luana Vallejos <luanalorenavallejos@gmail.com>
2023-08-01 01:48:10 -0300

    finished responsive in experience section

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,9 +5,9 @@ type Props = {}
 
 export default function ({}: Props) {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] 
-        md:w-[600px] xl:w-[900px] snap-center hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden mt-20">
-        <motion.img 
+    <article className="flex flex-col rounded-lg items-center space-y-3 md:space-y-7 shrink-0 w-64 space-x-4 
+        md:w-[600px] xl:w-[900px] snap-center cursor-pointer transition-opacity duration-200 overflow-hidden mt-20">
+        {/* <motion.img 
             initial={{
                 y:-100,
                 opacity: 0
@@ -20,31 +20,14 @@ export default function ({}: Props) {
             src="https://i.imgur.com/D41Pm3w.jpg"
             className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
             alt=""
-        />
-        <div className="px-0 md:px-10">
+        /> */}
+        <div className="pr-5 md:px-10 max-w-3 break-words">
             <h4 className="text-4xl font-light">React Developer</h4>
             <p className="font-bold text-2xl mt-1">InfinixSoft</p>
-            <div className="flex space-x-2 my-2">
-                <img 
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/768px-Typescript_logo_2020.svg.png" 
-                    className="h-10 w-10 rounded-full"
-                    alt=""/>
-                <img 
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/768px-Typescript_logo_2020.svg.png" 
-                    className="h-10 w-10 rounded-full "
-                    alt=""/>
-                <img 
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/768px-Typescript_logo_2020.svg.png" 
-                    className="h-10 w-10 rounded-full "
-                    alt=""/>
-            </div>
-            <p className="uppercase py-5 text-pink">April 2022 - Present</p>
-            <ul className="list-disc space-y-4 ml-5 text-lg">
-                <li>Develop</li>
-                <li>Develop</li>
-                <li>Develop</li>
-                <li>Develop</li>
-            </ul>
+            <p className="uppercase py-5 text-pink">March 2022 - April 2023</p>
+            <p className='text-xs md:text-xl'>Developing and maintaining the user interface. Optimizing the user experience. SEO best practices. Implementing responsive design on mobile websites.
+            Integration with APIs and databases.
+            </p>
         </div>
     </article>
   )
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -11,10 +11,8 @@ export default function WorkExperience({}: Props) {
     whileInView={{opacity:1}}
     transition={{duration:1.5}}
     className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-pink text-2xl font-press2p">Experience</h3>
-      <div className="w-full flex space-x-5overflow-x-scroll overflow-y-hidden p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray/20 scrollbar-thumb-pink/80">
-        <ExperienceCard/>
-        <ExperienceCard/>
+      <h3 className="absolute top-24 uppercase lg:tracking-[20px] tracking-[15px] text-pink lg:text-2xl text-xl font-press2p">Experience</h3>
+      <div className="w-full flex space-x-2overflow-x-scroll overflow-y-hidden p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray/20 scrollbar-thumb-pink/80 xl:overflow-x-hidden">
         <ExperienceCard/>
         <ExperienceCard/>
       </div>
