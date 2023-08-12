64e3258206a155cc8bf4a4a3c3daa56e77a02567
Luana Vallejos <luanalorenavallejos@gmail.com>
2023-08-11 23:48:48 -0300

    few changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { IProject } from '../types/IResponse'
 import Link from "next/link"
+import Image from 'next/image'
 import {motion} from "framer-motion"
 
 type Props = {
@@ -21,23 +22,23 @@ export default function Projects({data}: Props) {
         {data?.map((project: IProject) =>(
           <div className="xl:mt-24 xl:w-screen flex-shrink-0 xl:mb-12 snap-center flex flex-col md:flex-row xl:flex-col
             space-y-5 items-center justify-center py-24 md:px-36 xl:scale-75 md:scale-100 scale-90" key={project.id}>
-            <motion.img
-              initial={{y:-300}}
-              transition={{duration:1.2}}
-              whileInView={{opacity:1, y: 0 }}
-              viewport={{once: true}}
-              src={project.image}
-              className="rounded-xl xl:h-72 md:h-40 md:w-72 h-28 w-52 xl:w-[600px] object-cover" alt="project image" />
-            <div className="flex flex-col items-center space-y-4 xl:space-y-10 
-            px-0 md:px-10 xl:max-w-6xl max-w-sm">
-              <h4 className="text-base md:text-xl xl:text-4xl font-semibold text-center">{project.name}</h4>
-              <div className="px-2 md:-p-10">
-                <p className='text-xs md:text-base xl:text-lg'>{project.description}</p>
-                <Link href={project.link} target="_blank">
-                  <button className='linkButton mt-4'>Link</button>
-                </Link>
-              </div>  
-            </div>
+              <div className="overflow-hidden object-cover">
+                <Image
+                  src={project.image}
+                  width={600}
+                  height={600}
+                  className="rounded-xl xl:h-80" alt="project image" />
+              </div>
+                <div className="flex flex-col items-center space-y-4 xl:space-y-10 
+                px-0 md:px-10 xl:max-w-6xl max-w-sm">
+                  <h4 className="text-base md:text-xl xl:text-4xl font-semibold text-center">{project.name}</h4>
+                  <div className="px-2 md:-p-10">
+                    <p className='text-xs md:text-base xl:text-lg'>{project.description}</p>
+                    <Link href={project.link} target="_blank">
+                      <button className='linkButton mt-4'>Link</button>
+                    </Link>
+                  </div>  
+                </div>
           </div>
         ))}
       </div>
