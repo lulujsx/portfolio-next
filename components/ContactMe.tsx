c8a59c7521ec711b96298949487edd14fff891a4
Luana Vallejos <luanalorenavallejos@gmail.com>
2023-08-31 23:48:20 -0300

    commented non used components

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,59 +1,59 @@
 
-import {AiFillGithub} from "react-icons/ai"
-import {AiOutlineMail} from "react-icons/ai"
-import {FaLinkedin} from "react-icons/fa"
-import {SiLetterboxd} from "react-icons/si"
-import {motion} from "framer-motion"
+// import {AiFillGithub} from "react-icons/ai"
+// import {AiOutlineMail} from "react-icons/ai"
+// import {FaLinkedin} from "react-icons/fa"
+// import {SiLetterboxd} from "react-icons/si"
+// import {motion} from "framer-motion"
 
-type Props = {
+// type Props = {
 
-}
+// }
 
-export default function ContactMe({}: Props) {
+// export default function ContactMe({}: Props) {
 
 
-  return (
-    <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl
-    px-10 xl:justify-evenly justify-center space-y-10 mx-auto items-center">
-        <h3 className="hidden xl:block absolute top-16 uppercase tracking-[20px] text-yellow text-2xl font-press2p">Contact</h3>
-        <motion.div 
-            initial={{
-                x: -200
-            }}
-            transition={{
-                duration: 1.2
-            }}
-            whileInView={{
-                opacity: 1,
-                x: 0
-            }}
-            viewport={{once:true}}
-            className="flex flex-col space-y-10">
-            <h4 className="text-4xl xl:text-4xl font-semibold text-center xl:tracking-[15px]">Let&apos;s <span className="decoration-pink/80 underline">connect!</span></h4>
-        </motion.div>
-        <motion.div
-            initial={{
-                x: 200
-            }}
-            transition={{
-                duration: 1.2
-            }}
-            whileInView={{
-                opacity: 1,
-                x: 0
-            }}
-            viewport={{once:true}}
-            className="flex flex-row items-center gap-5 text-pink md:ml-7 xl:ml-0">
-            <a href="https://github.com/lulujsx" target="_blank">
-                <AiFillGithub className="text-xl xl:text-7xl"/>
-            </a>
-            <a href="https://www.linkedin.com/in/luanavallejos/" target="_blank">
-                <FaLinkedin className="text-xl xl:text-7xl"/>
-            </a>
-            <a href="mailto:luanalorenavallejos@gmail.com" target="_blank">
-                <AiOutlineMail className="text-xl xl:text-7xl"/>
-            </a>
-        </motion.div>
-    </div>
-  )
-}
+//   return (
+//     <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl
+//     px-10 xl:justify-evenly justify-center space-y-10 mx-auto items-center">
+//         <h3 className="hidden xl:block absolute top-16 uppercase tracking-[20px] text-yellow text-2xl font-press2p">Contact</h3>
+//         <motion.div 
+//             initial={{
+//                 x: -200
+//             }}
+//             transition={{
+//                 duration: 1.2
+//             }}
+//             whileInView={{
+//                 opacity: 1,
+//                 x: 0
+//             }}
+//             viewport={{once:true}}
+//             className="flex flex-col space-y-10">
+//             <h4 className="text-4xl xl:text-4xl font-semibold text-center xl:tracking-[15px]">Let&apos;s <span className="decoration-pink/80 underline">connect!</span></h4>
+//         </motion.div>
+//         <motion.div
+//             initial={{
+//                 x: 200
+//             }}
+//             transition={{
+//                 duration: 1.2
+//             }}
+//             whileInView={{
+//                 opacity: 1,
+//                 x: 0
+//             }}
+//             viewport={{once:true}}
+//             className="flex flex-row items-center gap-5 text-pink md:ml-7 xl:ml-0">
+//             <a href="https://github.com/lulujsx" target="_blank">
+//                 <AiFillGithub className="text-xl xl:text-7xl"/>
+//             </a>
+//             <a href="https://www.linkedin.com/in/luanavallejos/" target="_blank">
+//                 <FaLinkedin className="text-xl xl:text-7xl"/>
+//             </a>
+//             <a href="mailto:luanalorenavallejos@gmail.com" target="_blank">
+//                 <AiOutlineMail className="text-xl xl:text-7xl"/>
+//             </a>
+//         </motion.div>
+//     </div>
+//   )
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import React, {useState, useEffect} from 'react'
 import Head from "next/head";
 import Link from "next/link";
 import About from "../components/About";
-import ContactMe from "../components/ContactMe";
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
